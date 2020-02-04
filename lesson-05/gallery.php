19c0ded0b08a8473f9e235a0d45268783d2c144d
iTeacher <test@test.ru>
2020-02-04 21:06:03 +0300

    добавлены примеры 4-го урока

--- lesson-05/gallery.php
+++ lesson-05/gallery.php
@@ -1,1 +1,28 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+<div class="container">
+  <h1 class="my-4">Фотогалерея</h1>
+  <div class="row">
+    <div class="col-4 mb-4">
+      <div class="card card-default">
+        <div class="card-header">Фото #1</div>
+        <img src="./images/634a526f.jpg" class="card-img-top" alt="">
+        <div class="card-body">
+          Тут название файла
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
