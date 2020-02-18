77c0d110ecd61e14938058ee41115a1ed0360756
iTeacher <test@test.ru>
2020-02-18 21:50:16 +0300

    добавлены примеры 9-го урока

--- lesson-09/robot.php
+++ lesson-09/robot.php
@@ -6,6 +6,26 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  <style>
+    table {
+      border: 1px solid;
+      border-collapse: collapse;
+    }
+
+    td {
+      border: 1px solid;
+      width: 15px;
+      height: 15px;
+    }
+
+    .center {
+      background: #ffc6c1;
+    }
+
+    .visited {
+      background: blue;
+    }
+  </style>
 </head>
 <body>
 <form action="./robot.php" method="post">
@@ -15,6 +35,22 @@
 </form>
 <hr>
 <?php
+
+  $fieldSize = 55;
+  $field = [];
+  $offset = floor($fieldSize / 2);
+
+  for ($i=0; $i<$fieldSize; $i++) {
+    $row = [];
+    for ($j=0; $j<$fieldSize; $j++) {
+      $row[] = [
+        "visits" => 0,
+        "order" => 0,
+      ];
+    }
+    $field[] = $row;
+  }
+
   $error = "";
   $allowedCharacters = ["N", "E", "S", "W"];
   if ($_SERVER["REQUEST_METHOD"] === "POST") {
@@ -23,6 +59,7 @@
       $x = 0;
       $y = 0;
       $distance = 0;
+      $counter = 0;
 
       if (count($code) % 2 === 0) {
         for ($i=0; $i<count($code)-1; $i+=2) {
@@ -30,14 +67,35 @@
           $steps = $code[$i + 1];
           if (in_array($dir, $allowedCharacters) && $steps > 0) {
             if ($dir === "N") {
+              for ($j=$y+1; $j<=$y+$steps; $j++) {
+                $field[$offset - $j][$x + $offset]["visits"]++;
+                $counter++;
+                $field[$offset - $j][$x + $offset]["order"] = $counter;
+              }
               $y += $steps;
             } else if ($dir === "S") {
+              for ($j=$y-1; $j>=$y-$steps; $j--) {
+                $field[$offset - $j][$x + $offset]["visits"]++;
+                $counter++;
+                $field[$offset - $j][$x + $offset]["order"] = $counter;
+              }
               $y -= $steps;
             } else if ($dir === "E") {
+              for ($j=$x+1; $j<=$x+$steps; $j++) {
+                $field[$offset - $y][$j + $offset]["visits"]++;
+                $counter++;
+                $field[$offset - $y][$j + $offset]["order"] = $counter;
+              }
               $x += $steps;
             } else {
+              for ($j=$x-1; $j>=$x-$steps; $j--) {
+                $field[$offset - $y][$j + $offset]["visits"]++;
+                $counter++;
+                $field[$offset - $y][$j + $offset]["order"] = $counter;
+              }
               $x -= $steps;
             }
+            //$field[$offset - $y][$x + $offset]++;
             $distance += $steps;
           } else {
             $error = "Введите корректную последовательность команд";
@@ -46,7 +104,8 @@
           }
         }
         if ($error === "") {
-          echo "<p>Робот достиг точки ($x;$y) и проехал $distance м.</p>";
+          $shortDistance = round(hypot($x, $y), 2);
+          echo "<p>Робот достиг точки ($x;$y) и проехал $distance м. Расстояние по прямой: $shortDistance</p>";
         }
       } else {
         $error = "Введите чётное кол-во символов";
@@ -57,6 +116,31 @@
   if ($error !== "") {
     echo "<p style='color: red'>$error</p>";
   }
+
+  echo "<table>";
+  for ($i=0; $i<$fieldSize; $i++) {
+    echo "<tr>";
+      for ($j=0; $j<$fieldSize; $j++) {
+        $visits = $field[$i][$j]["visits"];
+        $class = "";
+
+        if ($i == $offset || $j == $offset) {
+          $class = "center";
+        }
+
+        if ($visits > 0) {
+          $class = "visited";
+        }
+
+        echo "<td class='$class'>";
+        if ($visits > 0) {
+          echo $visits;
+        }
+        echo "</td>";
+      }
+    echo "</tr>";
+  }
+  echo "</table>";
 ?>
 </body>
 </html>
