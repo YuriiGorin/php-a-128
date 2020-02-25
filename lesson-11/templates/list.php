df4981041aec57847545ccd53d732aa38d87e6b4
iTeacher <test@test.ru>
2020-02-25 20:19:16 +0300

    добавлены примеры 11-го урока

--- lesson-11/templates/list.php
+++ lesson-11/templates/list.php
@@ -1,4 +1,24 @@
 <h1 class="my-4">Список студентов</h1>
+<form action="/lesson-11/index.php">
+  <div class="card">
+    <div class="card-body">
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="form-group">
+            <label for="min">Возраст от</label>
+            <input id="min" type="text" class="form-control">
+          </div>
+        </div>
+        <div class="col-sm-3">
+
+        </div>
+        <div class="col-sm-3">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
 <table class="table table-bordered table-hover">
   <tr>
     <th>Имя</th>
